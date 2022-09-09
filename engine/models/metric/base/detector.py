73a8a58a955005412c3b8e2a11196e22410a8c24
Superskyyy <superskyyy@outlook.com>
2022-09-09 14:13:27 -0400

    Fix licenses
    Signed-off-by: Superskyyy <superskyyy@outlook.com>

--- engine/models/metric/base/detector.py
+++ engine/models/metric/base/detector.py
@@ -1,8 +1,32 @@
+#  Copyright 2022 SkyAPM org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from abc import ABC, abstractmethod
 from typing import Union
-from datetime import datetime
-from dateutil.parser import parse
-import numpy as np
+
 
 class BaseDetector(ABC):
     """Abstract class for Detector, supporting for customize detector."""
@@ -26,12 +50,10 @@ class BaseDetector(ABC):
 
     @abstractmethod
     def fit(self, X: float):
-
         return NotImplementedError
 
     @abstractmethod
     def score(self, X: float) -> float:
-
         return NotImplementedError
 
     def fit_score(self, timestamp: str, X: Union[float, int]) -> float:
--- engine/models/metric/detector/spot_detector.py
+++ engine/models/metric/detector/spot_detector.py
@@ -1,7 +1,35 @@
-from ..base import BaseDetector
-import numpy as np
+#  Copyright 2022 SkyAPM org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 from math import log
+
+import numpy as np
 from scipy.optimize import minimize
+
+from ..base import BaseDetector
 
 np.seterr(divide="ignore", invalid="ignore")
 
